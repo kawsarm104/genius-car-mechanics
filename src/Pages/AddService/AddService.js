906063a312e070d1ea55f1afec867d90b7de0e18
kawsarm104 <kawsarm104@gmail.com>
2021-10-24 13:00:35 +0600

    sending data to database with the help of axios successfull

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,13 +1,25 @@
+import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
-import './AddService.css'
+import './AddService.css';
 const AddService = () => {
       const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = data => {
+    console.log(data)
+    axios.post('http://localhost:3001/services', data)
+      .then(res => {
+        if (res.data.insertedId) {
+        alert('data inserted successfully')
+      }
+      })
+    
+    
+  };
+
     return (
-        <div className="add-service">
-            <h2>Please add a service</h2>
-              <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="add-service">
+      <h2>Please add a service</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <input placeholder="Name" {...register("firstName", { required: true, maxLength: 20 })} />
       <textarea placeholder="Description" {...register("Description", {required: true})} />
       <input placeholder="Price" type="number" {...register("price", { required: true })} />
